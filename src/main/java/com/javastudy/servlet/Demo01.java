143f4f3a111818d00b74bc7b3ad73b04ba4d2108
JavaDev2001 <114137458+JavaDev2001@users.noreply.github.com>
2022-09-24 11:23:08 +0800

    pull commit

--- src/main/java/com/javastudy/servlet/Demo01.java
+++ src/main/java/com/javastudy/servlet/Demo01.java
@@ -10,5 +10,6 @@ public class Demo01 {
         System.out.println("hot-fix  test");
         System.out.println("push test");
         System.out.println("push ssh");
+       System.out.println("pull ssh");
     }
 }
